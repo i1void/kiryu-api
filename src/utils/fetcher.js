f7784547a1e52265a850594555ad3284bbcb8884
Void <152130497+i1void@users.noreply.github.com>
2026-03-13 21:46:57 +0700

    .

--- src/utils/fetcher.js
+++ src/utils/fetcher.js
@@ -1,1 +1,15 @@
+const axios = require("axios");
 
+const fetcher = axios.create({
+  baseURL: "https://v1.kiryuu.to",
+  timeout: 15000,
+  headers: {
+    "User-Agent":
+      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "id-ID,id;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Referer": "https://v1.kiryuu.to/",
+  },
+});
+
+module.exports = fetcher;
